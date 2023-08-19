52fe76dfcd8b1750dbe396ef00e13cbf54b55d8c
Seki Ryota <sekiryouta749@gmail.com>
2023-08-19 10:10:09 +0000

    投稿機能,一覧機能の実装

--- app/controllers/valorants_controller.rb
+++ app/controllers/valorants_controller.rb
@@ -1,20 +1,21 @@
 class ValorantsController < ApplicationController
+
   def index
     @valorant = Valorant.new
+    @valorants = Valorant.all.page(params[:page]).per(10).order(created_at: :desc)
 
   end
 
   def create
     @valorant = Valorant.new(valorant_params)
     @valorant.save
-    redirect_to valorants_index_path
+    redirect_to valorants_path
   end
 
   def show
   end
 
   def new
-    @valorant = Valorant.new
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  
-  root to: 'tops#top'
-  
-  get 'valorants/index'
-  get 'valorants/show'
-  get 'valorants/new'
-  get 'valorants/edit'
 
-  
+  root to: 'tops#top'
+
+  resources :valorants
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
